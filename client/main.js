39da9e83a4834d32c0e12b941ccf6b72c73a4292
Nicholas O'Brien <ndo9903@rit.edu>
2016-09-04 15:02:39 -0700

    Made it look ALOT better

--- client/main.js
+++ client/main.js
@@ -1,18 +1,25 @@
 import { Template } from 'meteor/templating';
+import { Session } from 'meteor/session';
 
 Players = new Mongo.Collection('players');
-
 
 START_WORTH = 10;
 WORTH_INCREMENT = 5;
 MAX_WORTH = 30;
 
+LEVEL_NAMES = ["N00B", "Casual", "Average", "Skilled", "Master"];
+
 
 import './main.html';
+
+Session.setDefault('prompt', 'login');
 
 Template.body.helpers({
   players(){
     return Players.find({});
+  },
+  promptPage(){
+    return Session.get('prompt');
   }
 });
 
@@ -25,6 +32,10 @@ Template.register.events({
             username: usernameVar,
             password: passwordVar
         });
+    },
+    'click #changeToLogin': function(event){
+      event.preventDefault();
+      Session.set('prompt', 'login');
     }
 });
 
@@ -34,11 +45,15 @@ Template.login.events({
         var usernameVar = event.target.loginUsername.value;
         var passwordVar = event.target.loginPassword.value;
         Meteor.loginWithPassword(usernameVar, passwordVar);
+    },
+    'click #changeToRegister': function(event){
+      event.preventDefault();
+      Session.set('prompt', 'register');
     }
 });
 
 Template.dashboard.events({
-    'click .logout': function(event){
+    'click #logout': function(event){
         event.preventDefault();
         Meteor.logout();
     }
@@ -54,7 +69,8 @@ Template.dashboard.rendered = function(){
       worth: START_WORTH,
       worth_level: START_WORTH,
       drink: 1,
-      total_drinks: 0
+      total_drinks: 0,
+      level_name: LEVEL_NAMES[0]
     }
   );
 };
@@ -71,7 +87,8 @@ Template.dashboard.helpers({
         worth: START_WORTH,
         worth_level: START_WORTH,
         drink: 1,
-        total_drinks: 0
+        total_drinks: 0,
+        level_name: LEVEL_NAMES[0]
       }
     );
     player = Players.findOne({name: Meteor.user().username});
@@ -80,7 +97,7 @@ Template.dashboard.helpers({
 });
 
 Template.dashboard.events({
-  'click .up-button'(event){
+  'click #up-button'(event){
     event.preventDefault();
 
     Players.update(
@@ -92,12 +109,13 @@ Template.dashboard.events({
         worth: (player.worth+1),
         worth_level: player.worth_level,
         drink: player.drink,
-        total_drinks : player.total_drinks + 1
+        total_drinks : player.total_drinks - 1,
+        level_name: LEVEL_NAMES[(player.worth_level/5)-2]
       }
     );
   },
 
-  'click .down-button'(event){
+  'click #down-button'(event){
     event.preventDefault();
 
     if(player.worth-- == 1){
@@ -126,12 +144,13 @@ Template.dashboard.events({
         worth: player.worth,
         worth_level: player.worth_level,
         drink: player.drink,
-        total_drinks : player.total_drinks + 1
+        total_drinks: player.total_drinks + 1,
+        level_name: LEVEL_NAMES[(player.worth_level/5)-2]
       }
     );
   },
 
-  'click .reset-button'(event){
+  'click #reset-button'(event){
     event.preventDefault();
 
     player.worth = START_WORTH;
@@ -147,7 +166,8 @@ Template.dashboard.events({
         worth: player.worth,
         worth_level: player.worth_level,
         drink: player.drink,
-        total_drinks : 0
+        total_drinks : 0,
+        level_name: LEVEL_NAMES[0]
       }
     );
   }
