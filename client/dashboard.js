3cf38e170ba41a4eb0837204ab9381012f535ee9
Nicholas O'Brien <ndo9903@rit.edu>
2016-09-05 16:06:35 -0700

    tried separating some stuff so its not just two big files...

---
+++ client/dashboard.js
@@ -0,0 +1,107 @@
+import { Template } from 'meteor/templating';
+
+
+Template.dashboard.events({
+    'click #logout': function(event){
+        event.preventDefault();
+        Meteor.logout();
+    }
+});
+
+Template.dashboard.helpers({
+  player(){
+    player = Players.findOne({name: Meteor.user().username});
+    if (player){
+      return player;
+    }
+    Players.insert(
+      {
+        name: Meteor.user().username,
+        worth: START_WORTH,
+        worth_level: START_WORTH,
+        drink: 1,
+        total_drinks: 0,
+        level_name: LEVEL_NAMES[0]
+      }
+    );
+    player = Players.findOne({name: Meteor.user().username});
+    return player;
+  }
+});
+
+Template.dashboard.events({
+  'click #up-button'(event){
+    event.preventDefault();
+
+    Players.update(
+      {
+        _id: Players.findOne({name: player.name})['_id']
+      },
+      {
+        name: player.name,
+        worth: (player.worth+1),
+        worth_level: player.worth_level,
+        drink: player.drink,
+        total_drinks : player.total_drinks - 1,
+        level_name: LEVEL_NAMES[(player.worth_level/5)-2]
+      }
+    );
+  },
+
+  'click #down-button'(event){
+    event.preventDefault();
+
+    if(player.worth-- == 1){
+      if(player.drink == 2){
+        if(player.worth_level != MAX_WORTH){
+          player.worth_level += WORTH_INCREMENT;
+          player.drink = 1;
+        }
+        else{
+          player.drink += 1;
+        }
+        player.worth = player.worth_level;
+      }
+      else{
+        player.drink += 1;
+        player.worth = player.worth_level;
+      }
+    }
+
+    Players.update(
+      {
+        _id: Players.findOne({name: player.name})['_id']
+      },
+      {
+        name: player.name,
+        worth: player.worth,
+        worth_level: player.worth_level,
+        drink: player.drink,
+        total_drinks: player.total_drinks + 1,
+        level_name: LEVEL_NAMES[(player.worth_level/5)-2]
+      }
+    );
+  },
+
+  'click #reset-button'(event){
+    event.preventDefault();
+
+    player.worth = START_WORTH;
+    player.worth_level = START_WORTH;
+    player.drink = 1;
+
+    Players.update(
+      {
+        _id: Players.findOne({name: player.name})['_id']
+      },
+      {
+        name: player.name,
+        worth: player.worth,
+        worth_level: player.worth_level,
+        drink: player.drink,
+        total_drinks : 0,
+        level_name: LEVEL_NAMES[0]
+      }
+    );
+  }
+});
--- client/main.js
+++ client/main.js
@@ -15,12 +15,23 @@ import './main.html';
 Session.setDefault('prompt', 'login');
 
 Template.body.helpers({
-  players(){
-    return Players.find({}).fetch();
-  },
   promptPage(){
     return Session.get('prompt');
   }
+});
+
+Template.playerSummary.helpers({
+  players(){
+    return Players.find({}).fetch();
+  }
+});
+
+Template.playerSummary.events({
+  'click #changeToLogin': function(event){
+    event.preventDefault();
+    Session.set('prompt', 'login');
+  },
+
 });
 
 Template.register.events({
@@ -49,126 +60,9 @@ Template.login.events({
     'click #changeToRegister': function(event){
       event.preventDefault();
       Session.set('prompt', 'register');
-    }
-});
-
-Template.dashboard.events({
-    'click #logout': function(event){
-        event.preventDefault();
-        Meteor.logout();
-    }
-});
-
-Template.dashboard.rendered = function(){
-  Players.update(
-    {
-      _id: Players.findOne({name: player.name})['_id']
     },
-    {
-      name: Meteor.user().username,
-      worth: START_WORTH,
-      worth_level: START_WORTH,
-      drink: 1,
-      total_drinks: 0,
-      level_name: LEVEL_NAMES[0]
+    'click #changeToSummary': function(event){
+      event.preventDefault();
+      Session.set('prompt', 'playerSummary');
     }
-  );
-};
-
-Template.dashboard.helpers({
-  player(){
-    player = Players.findOne({name: Meteor.user().username});
-    if (player){
-      return player;
-    }
-    Players.insert(
-      {
-        name: Meteor.user().username,
-        worth: START_WORTH,
-        worth_level: START_WORTH,
-        drink: 1,
-        total_drinks: 0,
-        level_name: LEVEL_NAMES[0]
-      }
-    );
-    player = Players.findOne({name: Meteor.user().username});
-    return player;
-  }
-});
-
-Template.dashboard.events({
-  'click #up-button'(event){
-    event.preventDefault();
-
-    Players.update(
-      {
-        _id: Players.findOne({name: player.name})['_id']
-      },
-      {
-        name: player.name,
-        worth: (player.worth+1),
-        worth_level: player.worth_level,
-        drink: player.drink,
-        total_drinks : player.total_drinks - 1,
-        level_name: LEVEL_NAMES[(player.worth_level/5)-2]
-      }
-    );
-  },
-
-  'click #down-button'(event){
-    event.preventDefault();
-
-    if(player.worth-- == 1){
-      if(player.drink == 2){
-        if(player.worth_level != MAX_WORTH){
-          player.worth_level += WORTH_INCREMENT;
-          player.drink = 1;
-        }
-        else{
-          player.drink += 1;
-        }
-        player.worth = player.worth_level;
-      }
-      else{
-        player.drink += 1;
-        player.worth = player.worth_level;
-      }
-    }
-
-    Players.update(
-      {
-        _id: Players.findOne({name: player.name})['_id']
-      },
-      {
-        name: player.name,
-        worth: player.worth,
-        worth_level: player.worth_level,
-        drink: player.drink,
-        total_drinks: player.total_drinks + 1,
-        level_name: LEVEL_NAMES[(player.worth_level/5)-2]
-      }
-    );
-  },
-
-  'click #reset-button'(event){
-    event.preventDefault();
-
-    player.worth = START_WORTH;
-    player.worth_level = START_WORTH;
-    player.drink = 1;
-
-    Players.update(
-      {
-        _id: Players.findOne({name: player.name})['_id']
-      },
-      {
-        name: player.name,
-        worth: player.worth,
-        worth_level: player.worth_level,
-        drink: player.drink,
-        total_drinks : 0,
-        level_name: LEVEL_NAMES[0]
-      }
-    );
-  }
 });
